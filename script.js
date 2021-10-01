3f994dd19bc07bab7352ff950737abf3f24bff17
leonardo <leonardo.siepierski@outlook.com>
2021-10-01 14:04:49 -0300

    Terminado

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const list = document.querySelector('.cart__items');
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -28,47 +26,32 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-// requisito 4
-
-function save() {
-  localStorage.setItem('checkout', document.querySelector('ol').innerHTML);
-}
-
-function recover() {
-  localStorage.getItem('checkout', document.querySelector('ol').innerHTML);
-}
-
-// requisito 5
-// repositório que me ajudou a realizar esse requisito: https://github.com/tryber/sd-015-a-project-shopping-cart/pull/94/files
+// requisito 5 soma
 
 const sumFunction = () => {
   let sum = 0;
-  document.querySelectorAll('li').forEach((e) => {
-    sum += parseFloat(e.innerText.split('$').pop());
+  document.querySelectorAll('li').forEach((element) => {
+    sum += parseFloat(element.innerText.split('$').pop());
   });
-  return sum.toFixed(2);
+  return parseFloat(sum.toFixed(2));
 };
 
-const returnSum = () => {
+// requisito 5 imprime total da soma
+
+const printSum = () => {
   document.querySelector('.total-price').innerText = sumFunction();
 };
 
-// requisito 6
+// requisito 4 salva no local storage
 
-const clearCart = () => {
-  document.querySelector('.empty-cart').addEventListener('click', () => {
-    list.innerHTML = '';
-    save();
-    returnSum();
-  });
-};
+const save = () => localStorage.setItem('cart', document.querySelector('ol').innerHTML);
 
-// requisito 3
+// requisito 3 remove item do carrinho
 
 function cartItemClickListener(event) {
   event.target.remove();
   save();
-  returnSum();
+  printSum();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -79,19 +62,84 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-// requisito 7
+// requisito 2 adiciona ao carrinho
 
-const loading = () => {
-  const loadingDiv = document.createElement('div');
-  loadingDiv.className = 'loading';
-  loadingDiv.innerHTML = 'loading...';
-  document.querySelector('body').appendChild(loadingDiv);
+const toCart = async (id) => {
+  const list = document.querySelector('.cart__items');
+  try {
+    const response = await fetch(`https://api.mercadolibre.com/items/${id}`);
+    const resolve = await response.json();
+    const { id: sku, title: name, price: salePrice } = await resolve;
+    const cartItem = await createCartItemElement({ sku, name, salePrice });
+    list.appendChild(cartItem);
+    save();
+    printSum();
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const removeLoading = () => document.querySelector('.loading').remove();
+// requisito 7 remove loading
+
+const endLoading = () => {
+  document.querySelector('.load').remove();
+};
+
+// requisito 1 lista produtos
+
+const listItems = async (item) => {
+  const items = document.querySelector('.items');
+  try {
+    await item.results.forEach((element) => {
+      const { id, title, thumbnail } = element;
+      const createItem = createProductItemElement({ sku: id, name: title, image: thumbnail });
+      items.appendChild(createItem);
+      createItem.lastChild.addEventListener('click', (event) => {
+        const idTarget = getSkuFromProductItem(event.target.parentNode);
+        toCart(idTarget);
+      });
+    });
+    endLoading();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// requisito 1 chama api com computador e lista itens chamando a função listitems
+
+const request = async () => {
+  try {
+    const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+    const resolve = await response.json();
+    listItems(resolve);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// requisito 4 carrega do local storage e adiciona listener aos itens
+
+const startingPage = () => {
+  document.querySelector('ol').innerHTML = localStorage.getItem('cart');
+  document.querySelectorAll('li')
+  .forEach((element) => element.addEventListener('click', cartItemClickListener));
+};
+
+// requisito 6 limpa carrinho
+
+const clearCart = () => {
+  const clear = document.querySelector('.empty-cart');
+  clear.addEventListener('click', () => {
+    document.querySelector('ol').innerHTML = '';
+    localStorage.setItem('cart', '');
+    printSum();
+  });
+};
 
 window.onload = () => {
+  request();
+  startingPage();
   sumFunction();
-  returnSum();
+  printSum();
   clearCart();
 };
